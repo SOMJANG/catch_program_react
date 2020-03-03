9a80f0465027d3b843603c28270e4cddba087f93
DonghyunJang <jjdh11117@gmail.com>
2020-03-03 19:26:04 +0900

    3회차 과제(과제 1번영화 상세페이지 꾸미기)

--- week_2/src/pages/MovieDetail.js
+++ week_2/src/pages/MovieDetail.js
@@ -12,24 +12,32 @@ export default class extends React.Component {
     .get(url) 
     .then(response => response.body) 
     .catch(error => error); 
+
+    if (movieDetail.screeningStatus === "open"){
+      movieDetail.screeningStatus = "상영중";
+    }
+    else if (movieDetail.screeningStatus === "finished"){
+        movieDetail.screeningStatus = "상영 종료";
+    }
+    if (movieDetail.screeningStatus === "ready"){
+        movieDetail.screeningStatus = "상영 예정";
+    }
     
     //TODO: 2. 브라우저에 임시로 저장하기 (state) 
     // this.setState({ // ... // }) 
     //불러온 데이터를 브라우저에 임시로 저장한다 
     this.setState({ movieDetail }); 
     console.log(movieDetail) 
+
+    
   }; 
   
   render() { 
     const { movieDetail } = this.state; 
     
-
     
     return ( 
     <div> 
-      <h3>영화 상세 페이지</h3> 
-      <div>상세한 영화에 대한 정보가 노출될 페이지입니다</div> 
-      
       {/* TODO: 3. 영화 1개에 대한 데이터 출력하는 코드 짜기 */ 
       <MovieItem 
       title={movieDetail.title} 
@@ -37,18 +45,58 @@ export default class extends React.Component {
       advanceRate={movieDetail.advanceRate} 
       advanceRateRank={movieDetail.advanceRateRank} 
       plot = {movieDetail.plot}
+      released = {movieDetail.released}
+      genres = {movieDetail.genres}
+      casts = {movieDetail.cast}
+      director = {movieDetail.director}
+      runtime = {movieDetail.runtime}
+      visitorRating = {movieDetail.visitorRating}
+      expertRating = {movieDetail.expertRating}
+      screenStatus = {movieDetail.screeningStatus}
+      
+
       />} 
       </div> 
+      
       ); 
     } 
   } 
-  const MovieItem = ({ title, posterUrl, advanceRate, advanceRateRank, plot }) => { 
+  const MovieItem = ({ title, advanceRate, advanceRateRank, posterUrl, plot , released, genres, casts, director, runtime, visitorRating,expertRating, screenStatus}) => { 
+    // console.log(screenStatus);
+    console.log(casts)
     return (
       <div>
-      <h3>{title}</h3>
-      <img width="300" src={posterUrl}/>
-      <div>줄거리 : {plot}</div> 
-      <div>예매율: {advanceRate}</div> 
-      </div> 
+      <div className="detail_page">
+        <div className="detail_info">
+          <h1>{title}</h1>
+          {/* <img width="300" src={posterUrl}/> */}
+          {/* <div>줄거리 : {plot}</div>  */}
+          <h3>개봉일: {released}</h3>
+          <h3>감독 : {director}</h3>
+          {/* <h3>배우 : {casts}</h3> */}
+          <h3>배우 : {casts? casts.map(cast => <span>{cast}    </span>):null}</h3>
+          <h3>장르 : {genres}</h3>
+          <h3>상영 여부 : {screenStatus}</h3>
+          <h3>상영시간 : {runtime}</h3>
+          <h3>관람객 평점 : {visitorRating}</h3>
+          <h3>전문가 평점 : {expertRating}</h3>
+          
+          <h3>예매율: {advanceRate}</h3> 
+          <h3>예매율 순위 : {advanceRateRank}</h3>
+        </div> 
+
+        <div className="posterImg">
+              <img className="postImg" src={posterUrl}/>
+        </div>
+        </div>
+
+        <div className="detail_page">
+
+          <div className="plot_div">
+              <h2>줄거리</h2>
+              <h3>{plot}</h3> 
+          </div>
+        </div>
+      </div>
     ); 
   };
